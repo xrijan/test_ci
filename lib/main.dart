d51e9bb16d2a67761eac24cefd87ba2974605324
Rijan Rayamajhi <96490513+xrijan@users.noreply.github.com>
2023-06-27 22:48:36 +0530

    pull_request

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter ;
+  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
